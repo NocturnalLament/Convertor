bd6a94d229909e8a21f9b753d98bf3c5309d5cc5
Bradley Allen <103772876+NocturnalLament@users.noreply.github.com>
2024-01-25 11:41:15 -0500

    Add length conversion options for millimeters and imperial units

--- src/Conversions/Length/InitialData.rs
+++ src/Conversions/Length/InitialData.rs
@@ -1,4 +1,5 @@
-use inquire::{Select};
+use inquire::{Text, Select};
+#[path = "Types/Imperial/Furlong.rs"] mod Furlong;
 pub fn get_length_type() {
     /* Actually Getting Whether or not user wants to convert from Metric or Imperial unlike Temperature
     Because of how many different types of units there are for length it makes more sense to have this extra step 
@@ -14,7 +15,26 @@ pub fn get_length_type() {
                         Ok(item) => {
                             match item {
                                 "millimeters" => {
-                                    println!("millimeters");
+                                    let mill_conv = Text::new("How many millimeters are you converting? ").prompt();
+                                    // match mill_conv {
+                                    //     Ok(mill) => {
+                                    //         match mill.parse::<f64>() {
+                                    //             Ok(mill_as_f) => {
+                                    //                 let mill_as_f = mill_as_f;
+                                    //                 let mill = Millimeter::new(mill_as_f);
+                                    //                 mill.show_centimeters();
+                                    //                 mill.show_meters();
+                                    //                 mill.show_kilometers();
+                                    //             },
+                                    //             Err(_) => {
+                                    //                 println!("Error");
+                                    //             }
+                                    //         }
+                                    //     },
+                                    //     Err(_) => {
+                                    //         println!("Error");
+                                    //     }
+                                    // }
                                 },
                                 "centimeters" => {
                                     println!("centimeters");
@@ -36,9 +56,46 @@ pub fn get_length_type() {
                     }
                 },
                 "imperial" => {
-                    let test = vec!["inches", "feet", "yards", "miles"];
-                    for item in test {
-                        println!("{}", item);
+                    let chosen_item = Select::new("Select a unit: ", vec!["Furlong","Chains","inches", "feet", "yards", "miles"]).prompt();
+                    match chosen_item {
+                        Ok(item) => {
+                            match item {
+                                "Furlong" => {
+                                    let mut furlong_loop_running: bool = true;
+                                                    while furlong_loop_running {
+                                                        Furlong::furlong_prompt()
+                                                        let mut continue_running = Select::new("Would you like to continue and convert another furlong amount? (y/n) ", vec!["y", "n"]).prompt();
+                                                    }
+                                            }
+                                        },
+                                        Err(_) => {
+                                            println!("Error");
+                                        }
+                                    }
+                                },
+                                "Chains" => {
+                                    println!("Chains");
+                                },
+                                "inches" => {
+                                    println!("inches");
+                                },
+                                "feet" => {
+                                    println!("feet");
+                                },
+                                "yards" => {
+                                    println!("yards");
+                                },
+                                "miles" => {
+                                    println!("miles");
+                                },
+                                _ => {
+                                    println!("Error");
+                                }
+                            }
+                        },
+                        Err(_) => {
+                            println!("Error");
+                        }
                     }
                 },
                 _ => {
